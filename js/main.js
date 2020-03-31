17898c0568fcfa7ead84dc78b17e9b4c8db5f5d8
Andrea De Martino <ademartino1992@gmail.com>
2020-03-31 17:05:44 +0200

    Fix: Surname position in array

--- js/main.js
+++ js/main.js
@@ -14,17 +14,18 @@ while (checkSurname){
     surname = (surname[0].toUpperCase()) + (surname.slice(1))
     // Aggiungo cognome all'array
     surnameList.push(surname); 
-    // Visualizzo la posizione del cognome nell'array
-    alert('Complimenti sig. ' + surname + ', in questo momento è il ' + surnameList.indexOf(surname) + '° della lista.');
     // Ottengo e visualizzo la lista ordinata alfabeticamente in modo crescente
     var orderedlist = surnameList.sort();
     alert('Ecco a lei la lista ordinata: ' + orderedlist + '.');
+    // Visualizzo la posizione del cognome nell'array
+    var surnamePosition = orderedlist.indexOf(surname) + 1
+    alert('Complimenti sig. ' + surname + ', in questo momento è il ' + surnamePosition + '° della lista.');
     console.log(orderedlist);
     
   }
   // Esco dal ciclo se il cognome digitato non è coerente
   else{
-    alert('Il cognome digitato non valido, arrivederci.');
+    alert('Il cognome digitato non è valido, arrivederci.');
     checkSurname = false;
   } 
 }
